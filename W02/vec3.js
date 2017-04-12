065a80c3c8bed2987143cc506d60e5ba35f63ca2
松崎　継生 <171x222x@istc-c-001.epc.center.kobe-u.ac.jp>
2017-04-12 17:07:38 +0900

    task2

--- W02/vec3.js
+++ W02/vec3.js
@@ -47,3 +47,9 @@ Vec3.prototype.mid = function()
     if(min != this.z && max != this.z) return this.z;	   
 }
 
+Vec3.prototype.AreaOfTriangle(v0,v1,v2)
+{
+    var va = Vec3(v1.x - v0.x, v1.y - v0.y, v1.z - v0.z);
+    var vb = Vec3(v2.x - v0.x, v2.y - v0.y, v2.z - v0.z);
+    return 0;
+}

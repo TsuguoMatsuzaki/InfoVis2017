f61590c54dfcaa1ee2499bced0067369999cb730
松崎　継生 <171x222x@istc-c-001.epc.center.kobe-u.ac.jp>
2017-04-12 17:41:40 +0900

    task2

---
+++ W02/func.js
@@ -0,0 +1,9 @@
+function AreaOfTriangle(v0,v1,v2)
+{
+    var va = Vec3(v1.x - v0.x, v1.y - v0.y, v1.z - v0.z);
+    var vb = Vec3(v2.x - v0.x, v2.y - v0.y, v2.z - v0.z);
+    var ab_va_2 = Math.pow(va.x,2) + Math.pow(va.y,2) + Math.pow(va.z,2);
+    var ab_vb_2 = Math.pow(vb.x,2) + Math.pow(vb.y,2) + Math.pow(vb.z,2);
+    var inpr_2 = Math.pow(va.x*vb.x + va.y*vb.y + va.z*vb.z,2);
+    return (Math.sqrt(ab_va_2*ab_vb_2-inpr_2))/2;
+}

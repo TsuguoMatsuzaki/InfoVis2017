febb53d355573aa65ea3e735f53cd7a21d1198f9
松崎　継生 <171x222x@istc-a-004.epc.center.kobe-u.ac.jp>
2017-04-18 14:28:50 +0900

    initial_commit

---
+++ W03/W03.js
@@ -0,0 +1,23 @@
+    <html>
+    <head>
+    <title>W04: Example 01</title>
+</head>
+    <body>
+    <script src="http://mrdoob.github.io/three.js/buid/three.min.js"></script>
+    <script src="main.js"></scropt>
+    <script>
+    var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(
+    fov,
+    aspect,
+    near,
+    far
+);
+
+
+camera.position.set(x,y,z);
+
+sceme.add(camera);
+</script>
+    </body>
+    </html>

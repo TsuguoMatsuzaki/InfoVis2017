565b7797c034ace58683610305c8f36759e710e7
松崎　継生 <171x222x@istc-a-004.epc.center.kobe-u.ac.jp>
2017-04-19 12:25:46 +0900

    commit

---
+++ W04/W04.js
@@ -0,0 +1,25 @@
+    <html>
+    <head>
+    <title>W04: Example 01</title>
+    </head>
+    <body>
+    <script src="http://mrdoob.github.io/three.js/buid/three.min.js"></script>
+    <script src="main.js"></script>
+    <script>
+    var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(
+    fov,
+    aspect,
+    near,
+    far
+);
+
+
+camera.position.set(x,y,z);
+
+sceme.add(camera);
+
+main();
+</script>
+    </body>
+    </html>
